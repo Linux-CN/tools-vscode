3d826d646dec96e9710157dea3ce037404770d77
bestony <bestony@linux.com>
2023-07-16 19:26:14 +0800

    feat: add extension

--- extension.js
+++ extension.js
@@ -1,34 +1,96 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const { Range } = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const axios = require("axios").default;
+const printf = require("fast-printf").printf;
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+
+
 function activate(context) {
+	const template = "<<<<<<<\r\n %2$s \r\n=======\r\n %1$s \r\n>>>>>>>";
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "lctt" is now active!');
+	let completionRequest = async function (content) {
+		const { token,host } = vscode.workspace.getConfiguration('lctt');
+		let data = JSON.stringify({
+			"source": content
+		});
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('lctt.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+		let config = {
+			maxBodyLength: Infinity,
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${token}`
+			}
+		};
+		return await axios.post(`${host}/api/v1/completions`, data, config);
+	}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from lctt!');
-	});
+	let translateSelection = vscode.commands.registerCommand("lctt.translate_selection", async function () {
+		const editor = vscode.window.activeTextEditor;
+		const text = editor.document.getText(editor.selection);
+		vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			title:"AI 翻译中..."
+		}, async(progress) => {
+			try {
+				let response = await completionRequest(text);
+				let {target,token} = response.data.data;
+				editor.edit(editBuilder => {
+					editBuilder.replace(editor.selection, printf(template,target, text));
+				})
+				progress.report({ increment: 100, message: "处理成功" })
 
-	context.subscriptions.push(disposable);
+				vscode.window.showInformationMessage(`翻译成功，本次消耗 ${token} 个 Token`);
+			} catch (error) {
+				if(error.response.status == 402){
+					vscode.window.showErrorMessage("翻译失败，你的 Token 不足，请先手动翻译文章，文章被确认后，自动增加 Token。");
+				}
+				if(error.response.status == 403){
+					vscode.window.showErrorMessage("翻译失败，你的 Token 权限不足，请前往后台添加 completion:create 权限。");
+				}
+				progress.report({ increment: 100, message: "" })
+			}
+		})
+
+	})
+	context.subscriptions.push(translateSelection);
+
+
+	let translateAll = vscode.commands.registerCommand("lctt.translate_all", function () {
+		const editor = vscode.window.activeTextEditor;
+		const text = editor.document.getText();
+		let fullPageRange = new Range(editor.document.lineAt(0).range.start, editor.document.lineAt(editor.document.lineCount - 1).range.end)
+
+		vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			title:"AI 翻译中..."
+		}, async(progress) => {
+			try {
+				let response = await completionRequest(text);
+				let {target,token} = response.data.data;
+				editor.edit(editBuilder => {
+					editBuilder.replace(fullPageRange, printf(template,target, text));
+				})
+				progress.report({ increment: 100, message: "✅ 处理成功" })
+
+				vscode.window.showInformationMessage(`✅ 翻译成功，本次消耗 ${token} 个 Token`);
+			} catch (error) {
+				console.error(error)
+				if(error.response.status == 402){
+					vscode.window.showErrorMessage("翻译失败，你的 Token 不足，请先手动翻译文章，文章被确认后，自动增加 Token。",{modal:true});
+				}
+				if(error.response.status == 403){
+					vscode.window.showErrorMessage("翻译失败，你的 Token 权限不足，请前往后台添加 completion:create 权限。",{modal:true},);
+				}
+				progress.report({ increment: 100, message: "" })
+			}
+		})
+	})
+	context.subscriptions.push(translateAll);
+
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
